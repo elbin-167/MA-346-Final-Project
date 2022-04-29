ca21196e3135d4e730a7ed2391d03ca01afd4e92
elbin-167 <99831843+elbin-167@users.noreply.github.com>
2022-04-28 20:46:37 -0400

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -52,29 +52,18 @@ aircraft_data = aircraft_data[aircraft_data['Percentage'].notna()]
 # In[7]:
 
 
-from IPython.display import HTML
-import os
-# def get_list(start,stop,set1):
-#     empty_list = []
-#     a = np.arange(start,stop,set1)
-#     for i in a:
-#         empty_list.append(i)
-#     return empty_list
 
 
-# dic = {"0-25%": get_list(0,0.25,0.000001),        
-#         "25-50%": get_list(0.25,0.5,0.000001),
-#         "50-75%": get_list(0.5,0.75,0.000001),
-#         "75-100%": get_list(0.75,1,0.000001),
-#         }
 st.title('Aircraft Accidents 2018-2022')
 choice = st.sidebar.selectbox("Manufacturer",aircraft_data.Make.unique())
 
 choice_manu = aircraft_data[aircraft_data['Make']==choice]
 choice_manu
 loc = choice_manu[['lat','lon']]
-
 st.map(loc)
+st.write("[Deepnote Code](https://deepnote.com/workspace/elbin-rojas-8c4a-d0bbe724-bfed-4f88-8960-a4bdd8cc5fde/project/MA-346-Final-Project-174ece8e-9da1-4371-a2cd-1c30546a5476/%2FMA-346-Final-Project%2Fnotebook.ipynb)")
+st.write("[Project Report](https://bentleyedu-my.sharepoint.com/:w:/g/personal/rojas_elbi_bentley_edu/EfUfhVoXJlBNlfoEL2uFW8wBpXTgo-Z6Cx_XmPumQ_JQ5A?e=7rALTS)")
+st.write("[Github Repository](https://github.com/elbin-167/MA-346-Final-Project)")
 
 
 # create a number range for the percentages and use the choice_manu as a basis for dividing the ranges
